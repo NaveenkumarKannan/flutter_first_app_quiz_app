1901101d9ced93486fbb88b989bab33fe1b4159e
NaveenkumarKannan <46358531+NaveenkumarKannan@users.noreply.github.com>
2020-01-27 00:06:11 +0530

    First Day Learning Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello'));
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+      ),
+      body: Text('This is a body text'),
+    ));
   }
 }
